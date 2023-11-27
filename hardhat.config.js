49db26084fd34bb1d96753d11fab9b99d396956d
Developer Localhost <developer@local.host>
2023-11-27 17:18:40 -0500

    Add network mumbai to hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,24 @@
 
 require("@nomicfoundation/hardhat-toolbox");
 
+require('dotenv').config();
+const fs = require("fs");
+const { ethers } = require('ethers');
+
+function mnemonicToAccount() {
+  // const mnemonic = fs.readFileSync('mnemonic.dat', 'utf-8').trim();
+  const mnemonic = process.env.PRVKEY_MNEMONIC;
+  const wallet = ethers.Wallet.fromPhrase(mnemonic);
+  return wallet.privateKey;
+}
+
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    mumbai: {
+      url: process.env.URL_RPC_MUMBAI,
+      accounts: [mnemonicToAccount()],
+      saveDeployments: false,
+    }
+  }
 };
